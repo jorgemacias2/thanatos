dc9500e72f43b9f0584bf0749a809a8770999265
jorgemacias2 <35404558+jorgemacias2@users.noreply.github.com>
2020-06-29 08:52:01 -0500

    First commit

---
+++ ui-thanatos/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">Hello World</div>
+  );
+}
+
+export default App;
