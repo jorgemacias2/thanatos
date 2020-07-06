75ea5ce74ced25f1b924ddecb70dcf469d5f2c6b
codingneat <daniel.vergara.echeverri@gmail.com>
2020-07-06 10:24:20 -0500

    SQL Alchemy Implementation

--- api-thanatos/app/main.py
+++ api-thanatos/app/main.py
@@ -1,10 +1,20 @@
 # uvicorn app.main:app --reload --port=8080
+from functools import lru_cache
 from typing import Optional
 from fastapi import Depends, FastAPI, Header, HTTPException
 
+from . import config
 from .routers import users
+from . import models
+from .database import engine
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+@lru_cache()
+def get_settings():
+    return config.Settings()
 
 app.include_router(
     users.router,
@@ -15,8 +25,8 @@ app.include_router(
 
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def read_root(settings: config.Settings = Depends(get_settings)):
+    return {"Hello": settings.app_name}
 
 
 @app.get("/items/{item_id}")
--- api-thanatos/app/routers/users.py
+++ api-thanatos/app/routers/users.py
@@ -1,16 +1,35 @@
-from fastapi import APIRouter, HTTPException
+from typing import List
+from fastapi import APIRouter, HTTPException, Depends
+from sqlalchemy.orm import Session
+
+from .. import crud, schemas, database
+
 
 router = APIRouter()
 
 
-@router.get("/")
+@router.get("/demo")
 async def read_users():
     return [{"name": "User Foo"}, {"name": "user Bar"}]
 
 
-@router.get("/{user_id}")
+@router.get("/demo/{user_id}")
 async def read_user(user_id: str):
     return {"name": "Fake Specific User", "user_id": user_id}
+
+
+@router.post("/", response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(database.get_db)):
+    db_user = crud.get_user_by_email(db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    return crud.create_user(db=db, user=user)
+
+
+@router.get("/", response_model=List[schemas.User])
+def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(database.get_db)):
+    users = crud.get_users(db, skip=skip, limit=limit)
+    return users
 
 
 @router.put(
@@ -18,6 +37,7 @@ async def read_user(user_id: str):
     tags=["custom"],
     responses={403: {"description": "Operation forbidden"}},
 )
+
 async def update_user(user_id: str):
     if user_id != "foo":
         raise HTTPException(status_code=403, detail="You can only update the user: foo")
