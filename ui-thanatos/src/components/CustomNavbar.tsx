22d29c55c92cbc05ec4227ce1d805940c8d26555
jorgemacias2 <35404558+jorgemacias2@users.noreply.github.com>
2020-07-07 15:23:12 -0500

    Route and signup style

--- ui-thanatos/src/components/CustomNavbar.tsx
+++ ui-thanatos/src/components/CustomNavbar.tsx
@@ -5,11 +5,11 @@ function CustomNavbar() {
   return (
     <div className="full-navbar">
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Navbar.Brand className="home-navbar" href="#home">RebuscApp</Navbar.Brand>
+      <Navbar.Brand className="home-navbar" href="/">RebuscApp</Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse className="full-navbar" id="responsive-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link className="nav-link" href="#features">Home</Nav.Link>
+            <Nav.Link className="nav-link" href="/">Home</Nav.Link>
             <NavDropdown title="User" id="collasible-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Profile</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">Your Account</NavDropdown.Item>
