6e9b82f114f47bbad7cc23fcd928c9aab03e5021
codingneat <daniel.vergara.echeverri@gmail.com>
2020-07-06 19:41:29 -0500

    Small Boilerplate Additions

--- ui-thanatos/src/App.tsx
+++ ui-thanatos/src/App.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import './App.scss';
+import CustomNavbar from './components/CustomNavbar';
 
 function App() {
   return (
+    <>
+    <CustomNavbar></CustomNavbar>
     <Switch>
       <Route exact path='/'>
         <div className="container">Home</div>
@@ -13,6 +16,7 @@ function App() {
         <div className="container">First Page</div>
       </Route>
     </Switch>
+    </>
   );
 }
 
